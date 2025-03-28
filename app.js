b8dcb3cf4aa33f002f4f1b1e191a6fa113f5950b
Shoaib khan <shoaibmangal@yahoo.com>
2025-03-29 00:07:31 +0500

    add files to js practice 05

---
+++ app.js
@@ -0,0 +1,25 @@
+//  OBJECTS IN JAVASCRIPTS
+
+// function Car (model, year){
+//     this.model = model;
+//     this.year = year;
+//     this.start = function(){
+//         return model + "is going to drop the boss";
+//     }
+// }
+
+//    document.write([car1,car2]);
+
+// let car1 = new Car ("honda", 2024);
+// let car2 = new Car ("toyota", 2020);
+
+
+
+let person = {
+    firstName: "shoaib",
+    lastName: "khan",
+    age: 23,
+    education: "bscs",
+}
+    console.log(person.firstName)
+    console.log(this[age])
