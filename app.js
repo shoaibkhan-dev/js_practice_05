77902ac0bfd0a0d0bbd9c61cfa1b40d93e3b46e8
Shoaib khan <shoaibmangal@yahoo.com>
2025-03-30 00:26:05 +0500

    add files to js practice 05

--- app.js
+++ app.js
@@ -15,41 +15,79 @@
 
 
 
-let person = {
-    firstName: "shoaib",
-    lastName: "khan",
-    age: 23,
-    education: "bscs",
-}
-    console.log(person.firstName)
+// let person = {
+//     firstName: "shoaib",
+//     lastName: "khan",
+//     age: 23,
+//     education: "bscs",
+// }
+//     console.log(person.firstName)
     
-    lastName = "akhtar";
+//     lastName = "akhtar";
 
-    console.log(person.lastName)
+//     console.log(person.lastName)
 
 
 
-    const myArr = [0, 1, 2, 3, 4, 5];
-    console.log(myArr[2]);
+//     const myArr = [0, 1, 2, 3, 4, 5];
+//     console.log(myArr[2]);
 
-    myArr.push(6);
-    console.log(myArr)
+//     myArr.push(6);
+//     console.log(myArr)
 
-    myArr.push(7);
-    console.log(myArr)
+//     myArr.push(7);
+//     console.log(myArr)
 
-    myArr.pop();
-    console.log(myArr)
+//     myArr.pop();
+//     console.log(myArr)
 
-    myArr.shift();
-    console.log(myArr)
+//     myArr.shift();
+//     console.log(myArr)
 
-    myArr.unshift(9);
-    console.log(myArr)
+//     myArr.unshift(9);
+//     console.log(myArr)
 
-    myArr.slice(1, 3);
-    console.log(myArr)
+//     myArr.slice(1, 3);
+//     console.log(myArr)
 
-    console.log(myArr.includes(8));
-    console.log(myArr.indexOf(4));
-    console.log(myArr.length)
+//     console.log(myArr.includes(8));
+//     console.log(myArr.indexOf(4));
+//     console.log(myArr.length)
+
+const name = prompt("enter your name");
+const age = prompt("enter your age");
+const mathsMarks = +prompt("enter your maths marks");
+const urduMarks = +prompt("enter your maths urdu");
+const englishMarks = +prompt("enter your maths english");
+
+
+
+const totalMarks = 300;
+alert(totalMarks);
+const obtainedMarks = mathsMarks + englishMarks + urduMarks;
+alert(obtainedMarks);
+const percentage = obtainedMarks * 100 / totalMarks;
+alert(percentage);
+
+
+if (percentage >= 90 && percentage <= 100){
+    alert("A+ Grade");
+    if (percentage >= 90){
+        alert("Reward him 10k")
+    }
+} else if (percentage >= 80 && percentage <= 89){
+    alert("A Grade");
+} else if (percentage >= 60 && percentage <= 79){
+    alert("B grade");
+} else if (percentage >= 50 && percentage <= 69){
+    alert("C grade");
+} else if (percentage <50){
+    alert("FAiled");
+}
+    
+
+
+
+
+
+
