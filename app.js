6ac9c266dbfb2f85f7aa446e6e781d08a75871b8
Shoaib khan <shoaibmangal@yahoo.com>
2025-03-29 23:34:44 +0500

    add files to js practice 05

--- app.js
+++ app.js
@@ -22,4 +22,31 @@ let person = {
     education: "bscs",
 }
     console.log(person.firstName)
-    console.log(this[age])
+    
+    lastName = "akhtar";
+
+    console.log(person.lastName)
+
+
+
+    const myArr = [0, 1, 2, 3, 4, 5];
+    console.log(myArr[2]);
+
+    myArr.push(6);
+    console.log(myArr)
+
+    myArr.push(7);
+    console.log(myArr)
+
+    myArr.pop();
+    console.log(myArr)
+
+    myArr.shift();
+    console.log(myArr)
+
+    myArr.unshift(9);
+    console.log(myArr)
+    
+    console.log(myArr.includes(8));
+    console.log(myArr.indexOf(4));
+    console.log(myArr.length)
