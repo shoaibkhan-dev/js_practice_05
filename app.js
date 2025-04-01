b36b0e5ec644be6d9a9384dc3626f5e3ffa6fe2d
Shoaib khan <shoaibmangal@yahoo.com>
2025-04-01 14:57:59 +0500

    add files to js practice 05

--- app.js
+++ app.js
@@ -114,17 +114,23 @@
 
 
 
-for (i = 1; i <= 10; i++){
-    console.log(`outer loop: ${i}`);
-    for (j = 1; j <= 10; j++){
-        console.log(i + '  *  ' + j + '  =  ' + i      *      j);
-    }
+// for (i = 1; i <= 10; i++){
+//     console.log(`outer loop: ${i}`);
+//     for (j = 1; j <= 10; j++){
+//         console.log(i + '  *  ' + j + '  =  ' + i * j);
+//     }
+// }
+
+
+
+
+
+
+
+
+for(i = 1; i <= 10; i++){
+   console.log(`Table Of: ${i}`);
+   for(j = 1; j <= 10; j++){
+     console.log(i + ' * ' + j + ' = ' + i*j);
+   }
 }
-
-
-
-
-
-
-
-
